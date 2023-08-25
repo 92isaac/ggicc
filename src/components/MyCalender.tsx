4d6db3bcbeb9c12f80fc265fffaeccbec66886fc
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-08-25 13:19:52 +0100

    now.json

--- src/components/MyCalender.tsx
+++ src/components/MyCalender.tsx
@@ -22,9 +22,9 @@ const ReservationCalendar: React.FC<CalendarProps> = ({time, price, id}) => {
         const dayOfWeek = date.getDay(); // 0 (Sunday) to 6 (Saturday)
         const minTime = new Date(date);
 
-        // dayOfWeek === 0 ? minTime.setHours(0, 0) : minTime.setHours(0, 0);
+        dayOfWeek === 0 ? minTime.setHours(0, 0) : minTime.setHours(0, 0);
         
-        if (dayOfWeek === 0 || dayOfWeek === 6) {
+        if (dayOfWeek === 1 || dayOfWeek === 6) {
           minTime.setHours(10, 0); // 10:00 AM for weekends
         } else {
           minTime.setHours(18, 0); // 6:00 PM for weekdays
@@ -37,10 +37,10 @@ const ReservationCalendar: React.FC<CalendarProps> = ({time, price, id}) => {
         const dayOfWeek = date.getDay(); // 0 (Sunday) to 6 (Saturday)
         const maxTime = new Date(date);
 
-        // dayOfWeek === 0 ? maxTime.setHours(0, 0) : maxTime.setHours(0, 0);
+        dayOfWeek === 0 ? maxTime.setHours(0, 0) : maxTime.setHours(0, 0);
 
 
-        if (dayOfWeek === 0 || dayOfWeek === 6) {
+        if (dayOfWeek === 1 || dayOfWeek === 6) {
           maxTime.setHours(13, 0); // 1:00 PM for weekends
         } else {
           maxTime.setHours(20, 0); // 9:00 PM for weekdays
@@ -51,9 +51,11 @@ const ReservationCalendar: React.FC<CalendarProps> = ({time, price, id}) => {
   
     return (
       <div className="p-4">
-        <h1 className="text-xl font-semibold mb-4">Initial Consultation</h1>
-        <p>Check out our availability and book the date and time that works for you</p>
-        <div className="md:flex justify-center gap-10 ">
+       <div className="ml-0 md:ml-16 lg:ml-20 my-12">
+       <h1 className="text-3xl font-semibold mb-4">Initial Consultation</h1>
+        <p className="">Check out our availability and book the date and time that works for you</p>
+       </div>
+        <div className="md:flex justify-evenly gap-10 ">
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Select Date and Time:</label>
           <DatePicker
